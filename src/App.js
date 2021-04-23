4ea2c29bb2250ffc7105d223d7fe2180ece05520
KshitijV97 <writetokshitij@gmail.com>
2021-04-23 20:17:46 +0530

    Able to add notes to list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+
+const ListItem = ({ content }) => {
+	return <div className='rounded'>{content}</div>;
+};
 
 function App() {
-	return <div className='App'>Hello</div>;
+	const [userInput, setUserInput] = useState('');
+	const [itemList, setItemList] = useState([]);
+	return (
+		<div className='container-fluid'>
+			<div id='searchboxContainer' className='row'>
+				<input
+					className='rounded'
+					type='text'
+					value={userInput}
+					onChange={(event) => {
+						setUserInput(event.target.value);
+					}}
+				/>
+				<button
+					className='rounded'
+					onClick={() => {
+						setItemList([...itemList, userInput]);
+						setUserInput('');
+					}}
+				>
+					+
+				</button>
+			</div>
+			<div id='notesContainer'>
+				{itemList.map((item, index) => {
+					return <ListItem content={item} key={index} />;
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
